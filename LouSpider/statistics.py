44fc27dd919f53002ace107ecfdb60b7aa7ac47d
tobinchen <tobinchen@tobinchens-iMac.local>
2015-09-26 19:48:35 +0800

    plot

--- LouSpider/statistics.py
+++ LouSpider/statistics.py
@@ -11,10 +11,12 @@ import matplotlib.pyplot as plt
 
 # In[111]:
 
-louDf=pd.read_csv("output.txt",header=None,names=['date','lou','square','number','amount','price'])
+#louDf=pd.read_csv("result.txt",header=None,names=['date','lou','square','number','amount','price'],parse_dates=True)
+louDf=pd.read_csv("result.txt",header=None,names=['date','lou','square','number','amount','price'],parse_dates=True,index_col=['date'])
 louDf.head(5)
-louDf.drop_duplicates(cols='date')
+#louDf.drop_duplicates(cols='date')
 
+print louDf.index
 # In[112]:
 
 xc=louDf[louDf['lou']=='新城']
@@ -22,11 +24,13 @@ xc.head(2)
 
 
 # In[113]:
-
-dt=xc.loc[:,['date','price']]
-dt=dt.set_index('date')
+dt=xc
+print dt
+dt=xc.loc[:,['date','number']]
+#dt=dt.set_index('date')
 dt=dt.sort()
 #dt.sort_index(by='price')
+
 dt[0:12]
 
 
@@ -39,14 +43,20 @@ dt[12:24]
 
 plt.figure()
 for n in range(2016-2006):
-    d=dt[n*12:(n+1)*12]
-    plt.plot(d,label=str(206+n))
-    print d
+    #d=dt[n*12:(n+1)*12]
+    #plt.plot(d,label=str(2006+n))
+    #print d
     #d.plot(secondary_y=True,legend=True)
+
+    #dt.ix[str(2006+n)].plot()
+
+    plt.plot(dt.ix[str(2006+n)],label=str(2006+n))
 plt.legend(loc='best')
 
-dt.plot()
+#dt.plot()
+
 plt.show()
 #plt.plot(dt)
 #plt.plot(xc.loc[12:23,['price']])
 
+pd.TimeGrouper(freq='m')
